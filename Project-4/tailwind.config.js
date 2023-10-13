d9f0741e79d141694e9784fd803e6ce8bfbf54f2
itsnajeeb <iamnajeebjung@gmail.com>
2023-10-13 19:08:57 +0530

    Milestone-1 Assignement

---
+++ Project-4/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      // fontFamily: {
+      //   cursive: ['Pacifico', 'cursive'],
+      // },
+      // boxShadow: {
+      //   'cursive-bottom': '0 2px 0 0 rgba(0, 0, 0, 0.2) inset',
+      // },
+    },
+  },
+  plugins: [],
+}
+
